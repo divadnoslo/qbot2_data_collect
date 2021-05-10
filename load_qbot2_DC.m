0f1f3edd2ab5cc9a767a4fcfdd6735cbbc8c9d81
David Olson <divadnoslo@gmail.com>
2021-05-10 09:27:09 -0700

    whatever

--- load_qbot2_DC.m
+++ load_qbot2_DC.m
@@ -6,7 +6,7 @@ clc
 
 %**************************************************************************
 % Set Desired (Max) Run Time
-D.t_end = 10;
+%D.t_end = 10;
 t_end = 10;
 D.Fs = 50;
 D.dt = 1 / D.Fs;
@@ -20,19 +20,11 @@ D.radius = D.diameter / 2;
 % To use Pre-Built Motion Profile or Keyboard Motor Controls
 % You MUST comment/uncomment to corresponding subsystems in Simulink!
 
-% Be sure your choice reflects here accordingly
-D.use_motion_profile = 0; 
-
-% Load Desired Motion Profile
-if (D.use_motion_profile == true)
-    D.plot_v_cmds = false;
-    qbot2_motion_gen;
-    fprintf('Pre-Built Motion Profile Enabled...  \n')
-else
-    fprintf('Keyboard Motor Controls Enabled...   \n')
-    D.v_cmds = [];
-    D.v_w = zeros(2, length(0 : D.dt : D.t_end));
-end
+D.plot_v_cmds = true;
+qbot2_motion_gen;
+v_cmds = D.v_cmds;
+t_end = D.t_end;
+fprintf('Pre-Built Motion Profile Loaded...  \n')
 
 disp('Press the "Monitor & Tune" button in the Simulink Hardware Tab to Begin')
 %**************************************************************************
